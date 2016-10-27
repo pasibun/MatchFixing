e1f67608858b55b0a282e916ec1352493f085c70
Matthijs\Pasibun <matthijs_hovestad@hotmail.com>
2016-10-27 14:14:46 +0200

    created profile screen

--- app/src/main/java/com/matchfixing/minor/matchfixing/Register.java
+++ app/src/main/java/com/matchfixing/minor/matchfixing/Register.java
@@ -25,7 +25,7 @@ public class Register extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.Register);
+        setContentView(R.layout.register);
 
         nameField = (EditText)findViewById(R.id.edtuserid);
         passwordField = (EditText)findViewById(R.id.edtpass);
