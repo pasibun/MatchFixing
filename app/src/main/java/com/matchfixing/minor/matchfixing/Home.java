1bbf78d00189a7a7989be3295997e85e3ed647ef
Matthijs\Pasibun <matthijs_hovestad@hotmail.com>
2016-10-27 12:23:08 +0200

    Created home screen, new buttons profiel, matchmaker, mijn groepen, mijn uitnodigingen, kalender

--- app/src/main/java/com/matchfixing/minor/matchfixing/Home.java
+++ app/src/main/java/com/matchfixing/minor/matchfixing/Home.java
@@ -2,16 +2,18 @@ package com.matchfixing.minor.matchfixing;
 
 /**
  * Created by jylti on 22-9-2016.
+ * Edited by Matthijs
  */
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class Home extends Activity {
 
-    String name, password, age, kaliber;
-    TextView nameTV, ageTV, passwordTV, kaliberTV;
+    String name;
+    TextView nameTV;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,18 +21,29 @@ public class Home extends Activity {
         setContentView(R.layout.welcome);
 
         nameTV = (TextView) findViewById(R.id.name_field);
-        ageTV = (TextView) findViewById(R.id.password_field);
-        passwordTV = (TextView) findViewById(R.id.age_field);
-        kaliberTV = (TextView) findViewById(R.id.kaliber_field);
 
         name = getIntent().getStringExtra("name");
-        password = getIntent().getStringExtra("password");
-        age = getIntent().getStringExtra("age");
-        kaliber = getIntent().getStringExtra("kaliber");
 
-        nameTV.setText("Welcome "+name);
-        passwordTV.setText("Your password is "+password);
-        ageTV.setText("Your age is "+age);
-        kaliberTV.setText("Kaliber: " + kaliber);
+        nameTV.setText("Welcome " + name);
+    }
+
+    public void calender_calender(View view) {
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+
+    public void invites_invites(View view) {
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+
+    public void group_group(View view) {
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+
+    public void matchmaker_matchmaker(View view) {
+        startActivity(new Intent(this, RegisterActivity.class));
+    }
+
+    public void profile_profile(View view) {
+        startActivity(new Intent(this, RegisterActivity.class));
     }
 }
